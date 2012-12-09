37a27aaca652c54fdb527b3a340eb8895a5aae72
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2012-12-09 17:03:45 -0500

    Reference to APPPATH in default config.

--- config/search.php
+++ config/search.php
@@ -1,6 +1,8 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php
+
+defined('SYSPATH') or die('No direct script access.');
 
 return array(
-	'index_path' => 'searchindex',
-	'use_english_stemming_analyser' => TRUE,
+    'index_path' => APPPATH . 'searchindex',
+    'use_english_stemming_analyser' => TRUE,
 );
